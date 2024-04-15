fc5489607e5e1e4b2f5d3477f1034daa28854551
eisemsi2 <96539950+eisemsi2@users.noreply.github.com>
2024-04-15 11:31:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/app/api/order/cancel', cancelorder);
 
 app.use((req,res,next) => {
     res.status(404).json({
-        message: 'bad request'
+        message: 'PLEASE REQUEST ONE OF THE FOLLOWING URIs AS IN README.'
     })
 });
 
